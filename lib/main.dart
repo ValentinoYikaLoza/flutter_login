311e1b0f3d81546abbc0e43c031c9192e6a14667
ValentinoYikaLoza <138173621+ValentinoYikaLoza@users.noreply.github.com>
2024-05-07 17:13:50 -0500

    ambos logins terminados

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:login/config/constants/environment.dart';
 import 'package:login/config/router/app_router.dart';
 import 'package:login/config/theme/app_theme.dart';
+import 'package:login/features/providers/auth_provider.dart';
 
 void main() async {
   await Environment.initEnvironment();
@@ -14,8 +15,24 @@ void main() async {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
+
+  @override
+  MainAppState createState() => MainAppState();
+}
+
+class MainAppState extends ConsumerState<MainApp> {
+  
+  //! agregar si se quiere sesion con limpieza de token
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   Future.microtask(() {
+  //     ref.read(authProvider.notifier).logOut();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
